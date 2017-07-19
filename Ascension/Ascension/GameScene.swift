418f82796b6a089af9924a7e4faec076825eafd2
Diego Lucero <iamdiegolucero@gmail.com>
2017-07-19 15:30:46 -0500

    Added Swipe controls w/basic physics
    Swipe left to go left, swipe right to go right, swipe up to jump and double jump

--- Ascension/Ascension/GameScene.swift
+++ Ascension/Ascension/GameScene.swift
@@ -11,10 +11,73 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var hero: Hero!
+    var hero: SKSpriteNode!
+    var moveX: CGFloat = 2
     
     override func didMove(to view: SKView) {
-        self.childNode(withName: "hero") as!Hero
+        /* Setup your scene here */
+        
+
+        hero = self.childNode(withName: "hero") as! SKSpriteNode
+        moveX = 0
+        
+        let swipeRight:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipedRight))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
+        
+        
+        let swipeLeft:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipedLeft))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
+        
+        
+        let swipeUp:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipedUp))
+        swipeUp.direction = .up
+        view.addGestureRecognizer(swipeUp)
+        
+        
+        let swipeDown:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipedDown))
+        swipeDown.direction = .down
+        view.addGestureRecognizer(swipeDown)
     }
     
+    func swipedRight(sender:UISwipeGestureRecognizer){
+        print("swiped right")
+        hero.physicsBody?.velocity.dx = 200
+        
+    }
+
+    func swipedLeft(sender:UISwipeGestureRecognizer){
+        print("swiped left")
+        hero.physicsBody?.velocity.dx = -200
+
+    
+    }
+    
+    func swipedUp(sender:UISwipeGestureRecognizer){
+        print("swiped up")
+        hero.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 25))
+    }
+    
+    func swipedDown(sender:UISwipeGestureRecognizer){
+        print("swiped down")
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        move()
+        
+        /* Grab current velocity */
+        let velocityY = hero.physicsBody?.velocity.dy ?? 0
+        
+        
+        /* Check and cap vertical velocity */
+        if velocityY > 500 {
+            hero.physicsBody?.velocity.dy = 500
+        }
+
+    }
+
+    func move() {
+        hero.position.x += moveX
+    }
 }
